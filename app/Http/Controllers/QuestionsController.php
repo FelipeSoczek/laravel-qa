5ed1bbd349a3b3eef915f735e3bd7fe24e3823d5
Felipe Cebulski Soczek <felipecebulskisoczek@gmail.com>
2019-12-30 17:26:50 -0300

    Extend the form functionality to be able to update question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -66,9 +66,10 @@ class QuestionsController extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function edit(Question $question)
+    public function edit(Question $question)  //edit($id)
     {
-        //
+        return view("questions.edit", compact('question'));
+        //$question = Question::findOrFail($id);
     }
 
     /**
@@ -78,9 +79,11 @@ class QuestionsController extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+        $question->update($request->only('title', 'body'));
+
+        return redirect('/questions')->with('success', "Your question has been updated.");
     }
 
     /**
--- resources/views/layouts/_messages.blade.php
+++ resources/views/layouts/_messages.blade.php
@@ -2,7 +2,7 @@
     <div class="alert alert-success alert-dismissible fade show" role="alert">
         <strong>Sucess!</strong> {{ session('success') }}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">$times;</span>
+            <span aria-hidden="true">&times;</span>
         </button>
     </div>
 @endif
